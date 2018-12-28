43fd197ba484a3954f1b3c62430e46db23e858e1
chulM <chlcjfals0122@hanmail.net>
2018-12-28 19:53:30 +0900

    initial commit.

---
+++ spring-server/src/main/java/com/websocket/chulm/EchoApplication.java
@@ -0,0 +1,11 @@
+package com.websocket.chulm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EchoApplication.class);
+    }
+}
